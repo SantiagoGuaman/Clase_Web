21ba188e6dfa79c8f961a87e7e380d35e3d67d02
FicctionZ <santiagogcan@gmail.com>
2024-02-14 23:15:33 -0500

    For React

--- src/main/java/com/tec/azuay/edu/controller/ProductosRestController.java
+++ src/main/java/com/tec/azuay/edu/controller/ProductosRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.tec.azuay.edu.models.services.IServicioProductos;
 
 @RestController
 @RequestMapping ("/api")
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class ProductosRestController {
 
 	@Autowired
